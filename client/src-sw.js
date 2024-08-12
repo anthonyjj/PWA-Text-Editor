eb115c84519028c37a2c2de5661834b55dceac0d
anthonyjj <156860966+anthonyjj@users.noreply.github.com>
2024-08-12 11:21:40 -0700

    fixed plugin problems

--- client/src-sw.js
+++ client/src-sw.js
@@ -28,16 +28,13 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
 registerRoute(
-({ request }) => {
-  return ['style', 'script', 'worker'].includes(request.destination);
-},
-new StaleWhileRevalidate({
-  cacheName: 'static-resources',
-  plugins: [
-    new CacheableResponsePlugin({
-      statuses: [0, 200],
-    }),
-  ],
-})
-); 
-
+  ({ request }) => ["style", "script", "worker"].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: "asset-cache",
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,40 +6,6 @@ const WorkboxPlugin = require('workbox-webpack-plugin');
 const path = require('path');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
-plugins: [
-  new HtmlWebpackPlugin({
-    template: './src/js/index.js',
-    title: 'Webpack Plugin',
-  }),
-  new MiniCssExtractPlugin(),
-  new WorkboxPlugin.GenerateSW()
-],
-
-new WebpackPwaManifest({
-  fingerprints: false,
-  inject: true,
-  name: "PWA Text Editor",
-  short_name: "JATE",
-  description: "Text Editor",
-  background_color: "#225ca3",
-  start_url: "/",
-  publicPath: "/",
-})
-// TODO: Add CSS loaders and babel to webpack.
-module: {
-  rules: [
-    {
-      test: /\.css$/i,
-      use: [MiniCssExtractPlugin.loader, 'css-loader'],
-    }
-  ]
-}
-
-new InjectManifest({
-  swSrc: "./src-sw,js",
-  swDest: "src-sw.js",
-}),
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -52,13 +18,57 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "JATE",
+      }),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Just Another Text Editor",
+        short_name: "JATE",
+        description: "Text Editor with offline capabilities using IndexedDB",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512], 
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
-
+// TODO: Add CSS loaders and babel to webpack.
     module: {
       rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        }
         
       ],
     },
   };
 };
+
